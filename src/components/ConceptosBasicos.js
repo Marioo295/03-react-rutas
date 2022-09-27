db0704163aca7fe60d2cf77ce3ce14db55ba6129
Marioo295 <mramosgaliana@gmail.com>
2022-09-27 17:17:34 +0200

    Rutas: sintaxis para declarar rutas

--- src/components/ConceptosBasicos.js
+++ src/components/ConceptosBasicos.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Acerca from '../pages/Acerca'
+import Contacto from '../pages/Contacto'
 
 const ConceptosBasicos = () => {
     return (
@@ -9,16 +11,15 @@ const ConceptosBasicos = () => {
                 {/* La ruta del home siempre tiene que estar al final en el switch para que carguen las anteriores rutas,
                 a no ser que tenga el atributo exact en el componente*/}
                 <Switch>
+                    {/* El componente Route es un componente contenedor */}
                     <Route exact path="/">
                         <h3>Home</h3>
                         <p>Bienvenidos al tema de las rutas en React</p>
                     </Route>
-                    <Route exact path="/acerca">
-                        <h3>Acerca</h3>
-                    </Route>
-                    <Route exact path="/contacto">
-                        <h3>Contacto</h3>
-                    </Route>
+                    <Route exact path="/acerca" component={Acerca}></Route> 
+                    {/* Hay dos maneras de definirlo
+                     <Route exact path="/contacto" component={Contacto}></Route> */}
+                    <Route exact path="/contacto" children={<Contacto></Contacto>}></Route>
                 </Switch>            
             </Router>
         </div>
